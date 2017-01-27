f6cb3c5fff09f1ddc9e2a298516cd0effe1efc6d
Applied-Programming <aniruddhastapas@gmail.com>
2017-01-28 01:12:54 +0530

    Update

--- src/AppPackage/MainClass.java
+++ src/AppPackage/MainClass.java
@@ -4,108 +4,82 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
 public class MainClass {
 
-    FileInputStream FIS;
-    BufferedInputStream BIS;
+    FileInputStream fileis;
+    BufferedInputStream buffis;
 
     public Player player;
-    public long pauseLocation;
-    public long songTotalLength;
-    public String fileLocation;
+    public long pause_location;
+    public long song_total_length;
+    public String file_location;
 
     public void Stop() {
-
         if (player != null) {
             player.close();
-            
-            pauseLocation=0;
-            songTotalLength=0;
-            
+
+            pause_location = 0;
+            song_total_length = 0;
             //MP3PlayerGUI.Display.setText(" ");
-            
         }
     }
 
     public void Pause() {
-
         if (player != null) {
             try {
-                pauseLocation = FIS.available();
+                pause_location = fileis.available();
                 player.close();
-            } catch (IOException ex) {
-
-            }
+            } catch (IOException ex) {}
         }
     }
 
     public void Play(String path) {
-
         try {
-            FIS = new FileInputStream(path);
-            BIS = new BufferedInputStream(FIS);
+            fileis = new FileInputStream(path);
+            buffis = new BufferedInputStream(fileis);
 
-           player = new Player(BIS);
-           
-           songTotalLength = FIS.available();
-           
-           fileLocation = path + "";
-        } catch (FileNotFoundException | JavaLayerException ex) {
+            player = new Player(buffis);
 
-        } catch (IOException ex) {
-            
-        }
+            song_total_length = fileis.available();
+
+            file_location = path + "";
+        } catch (FileNotFoundException | JavaLayerException ex) {}
+        catch (IOException ex) {}
 
         new Thread() {  //Seperate thread to play music
 
             public void run() {
                 try {
                     player.play();
-                } catch (JavaLayerException ex) {
-
-                }
+                } catch (JavaLayerException ex) {}
             }
         }.start();
     }
 
     public void Resume() {
-
         try {
-            FIS = new FileInputStream(fileLocation);
-            BIS = new BufferedInputStream(FIS);
-
-            player = new Player(BIS);
-            
-            FIS.skip(songTotalLength -  pauseLocation);
-        } catch (JavaLayerException ex) {
-
-        } catch (FileNotFoundException ex) {
-            
-        } catch (IOException ex) {
+            fileis = new FileInputStream(file_location);
+            buffis = new BufferedInputStream(fileis);
+            player = new Player(buffis);
+            fileis.skip(song_total_length - pause_location);
             
         }
-
+        catch (JavaLayerException ex) {}
+        catch (FileNotFoundException ex) {}
+        catch (IOException ex) {}
         new Thread() {  //Seperate thread to play music
 
             public void run() {
                 try {
                     player.play();
-                    
-                    if(player.isComplete() && MP3PlayerGUI.count == 1){
-                        Play(fileLocation);
+                    if (player.isComplete() && MP3PlayerGUI.count == 1) {
+                        Play(file_location);
                     }
-                    
-                            
-                } catch (JavaLayerException ex) {
-
-                }
+                } catch (JavaLayerException ex) {}
             }
         }.start();
     }
 }
-
